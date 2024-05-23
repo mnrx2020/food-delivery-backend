a0478399606c312af62ed38ff81c5e7633830ada
mnrx2020 <jamesmbere@gmail.com>
2024-05-23 17:30:46 +0300

    update change

--- server.js
+++ server.js
@@ -1,49 +1,55 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
-import userRouter from "./routes/userRoute.js"
-import "dotenv/config"
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
+import express from 'express';
+import cors from 'cors';
+import { connectDB } from './config/db.js';
+import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import 'dotenv/config';
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Define __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// App config
+const app = express();
+const port = 4000;
 
-//app config
-const app = express()
-const port = 4000
-
-//middleware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use(cors({
+  origin: [
+    'http://localhost:4000',
+    'https://mnrx-mern-food-delivery-frontend-app.onrender.com',
+    'https://mnrx-mern-food-delivery-admin-app.onrender.com'
+  ]
+}));
+app.use(express.urlencoded({ extended: false }));
 
-    origin:["http://localhost:4000",
-    "https://mnrx-mern-food-delivery-frontend-app.onrender.com",
-    "https://mnrx-mern-food-delivery-admin-app.onrender.com"]
+// DB connection
+connectDB();
 
-}))
-app.use(express.urlencoded({extended:false}))
-//db connection
-connectDB()
+// API endpoints
+app.use('/api/food', foodRouter);
+app.use('/images', express.static('uploads'));
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
-//api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static("uploads"))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
-
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+app.get('/', (req, res) => {
+  res.send('API Working');
+});
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Catch-all handler to return the `index.html` file for any request that doesn't match an existing route or file
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
-app.listen(port, ()=>{console.log(`Server Started on http://localhost:${port}`)})
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
